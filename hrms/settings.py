a4aaf673045ae81be29045e01cba5f10cce2d502
MaximSh <baksbanni31@gmail.com>
2023-12-29 10:38:30 +0200

    add company app

--- hrms/settings.py
+++ hrms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cf1urvgpdu^!!uqkg(y829+%he$t5@2m!e#4l=m3*z*zffl6ge'
+SECRET_KEY = env.str('HRMS_DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("HRMS_DEBUG")

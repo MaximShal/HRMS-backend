8c1225a6abf5d2c776a0e958ac10d61106bc5c77
MaksimYakovenko <maksim13yakovenko@gmail.com>
2023-12-30 19:36:02 +0200

    Initial Commit

--- company/models.py
+++ company/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Company(AbstractUser):
+    name = models.CharField(max_length=255)
+    address = models.TextField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    industry = models.CharField(max_length=100)

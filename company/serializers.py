e033586624c74e7edb354c5c05a351b30fb0c841
MaksimYakovenko <maksim13yakovenko@gmail.com>
2024-01-15 14:30:18 +0200

    draft login api

--- company/serializers.py
+++ company/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
-from .models import Users
+from django.contrib.auth import authenticate
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Users
-        fields = ('id', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True,
+                                     style={'input_type': 'password'})
+
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'),
+                                email=email, password=password)
+
+            if not user:
+                raise serializers.ValidationError(
+                    'Unable to log in with provided credentials.',
+                    code='authorization')
+        else:
+            raise serializers.ValidationError(
+                'Must include "email" and "password".', code='authorization')
+
+        data['user'] = user
+        return data
--- company/views.py
+++ company/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_jwt.settings import api_settings
+from .serializers import LoginSerializer
 
-# Create your views here.
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+        return Response({'token': token, 'message': 'Login successful'},
+                        status=status.HTTP_200_OK)
